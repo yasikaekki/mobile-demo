2fe47a92d272d75f7b447af12d094adec3cd6fe2
Alif Akbar Fitrawan <alief.ring3@gmail.com>
2021-06-10 06:52:30 +0700

    [feat] - gitignore - first project function

---
+++ lib/extensions/extensions.dart
@@ -0,0 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:bwa_flutix/models/models.dart';
+import 'package:bwa_flutix/services/services.dart';
+
+part 'firebase_user_extension.dart';
---
+++ lib/shared/shared_value.dart
@@ -0,0 +1,6 @@
+part of 'shared.dart';
+
+String apiKey = '40f5f75e66d72e1a56839f1bb415319c';
+String imageBaseURL = 'https://image.tmdb.org/t/p/';
+
+PageEvent prevPageEvent;
